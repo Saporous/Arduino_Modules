bdb2adf68e1b871eca8b92b7b3608c6778e6ecbc
Jeffrey C <chen.jeffrey.r@gmail.com>
2015-06-26 15:33:05 -0700

    rename ADC test to just ADC, update readme, change strobelight interrupt to use attachInterrupt() function

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # Arduino_Modules
 Code for applications of specific Arduino Modules implemented on either the Arduino Leonardo or Nano boards.
+
+## ADC 
+Demonstration of the Arduino's ADC capability on pin A7, works on Nano.
+
+## Arduino-PID-Library
+A library created by Brett Beauregard <br3ttb@gmail.com> brettbeauregard.com
+that implements PID using A0 to control A3.
+
+## HC-16 LiquidCrystal_I2C For Arduino 1602LCD Display
+A basic implementation of the SainSmart HC-16 breakout board. It uses I2C
+to communicate with an Arduino board to display messages on a LCD display.
+It currently works with a 20x4 character display, but may be modified to
+work on a 16x2 character display. It displays "Hello World!" on two lines,
+with a number counter on the bottom right corner.
+
+## lcdthermocouple
+A combined implementation of the HC-16 breakout board with the MAX6675
+thermocouple high temperature sensor that displays the temperature read
+via SPI from the Maxim breakout board on a LCD display via I2C.
+
+## lcdthermocouplePID
+An extension of the "lcdthermocouple" project that adds PID control from the
+"Arduino-PID-Library" on the default pins A0 and A3.
+
+## MAX6675-library
+A library created for a variant of the Adafruit MAX6675 breakout board that 
+communicates read values from a high-temperature thermocouple to an Arduino
+board via SPI with S0 on D4, SC on D5, and CS on D6.
+
+## strobeLight
+A project utilizing a potentiometer, onboard LED, and Arduino interrupt
+functionality to flash the onboard LED every time a falling edge is detected
+on the interrupt pin 
--- strobeLight/strobeLight/strobeLight.ino
+++ strobeLight/strobeLight/strobeLight.ino
@@ -2,13 +2,14 @@
   strobeLight - By Jeffrey Chen
   
 */
-#if not (defined(__AVR_ATmega32U4__) || defined(__AVR_ATmega328__))
+#if not (defined(__AVR_ATmega32U4__) || defined(__AVR_ATmega328P__))
   #error Your board type is not supported!
 #endif
 
-#if (defined(__AVR_ATmega328__))
+#if (defined(__AVR_ATmega328P__))
   #define LED_PIN 13
-  #define POT_PIN 7
+  #define POT_PIN 3
+  #define VCC_PIN 4
   #define INT_PIN 2
 #endif
 #if (defined(__AVR_ATmega32U4__))
@@ -17,27 +18,35 @@
   #define INT_PIN 3
 #endif
 
-int duration;
-
+float duration;
+bool blinkFlag;
 // the setup function runs once when you press reset or power the board
 void setup() {
   Serial.begin(9600);
   // initialize digital pin 13 as an output.
   pinMode(LED_PIN, OUTPUT);
   pinMode(POT_PIN, INPUT);
-  pinMode(INT_PIN, INPUT);
-  attachInterrupt(0, blink, FALLING);
+  pinMode(INT_PIN, INPUT_PULLUP);
+  pinMode(VCC_PIN, OUTPUT);
+  digitalWrite(VCC_PIN, HIGH);
+  attachInterrupt(0, blink, CHANGE);
 }
 
 void blink(){
-  // get duration based upon pot input
-  duration = analogRead(POT_PIN)*6;
-  digitalWrite(LED_PIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(duration);              // wait for a second
-  digitalWrite(LED_PIN, LOW);    // turn the LED off by making the voltage LOW
+  blinkFlag = 1;
 }
 
 // the loop function runs over and over again forever
 void loop() {
+  if(blinkFlag){
+    blinkFlag = 0;
+    digitalWrite(LED_PIN, LOW);   // turn the LED on (HIGH is the voltage level)
+    delay(duration);              // wait for a second
+    digitalWrite(LED_PIN, HIGH);    // turn the LED off by making the voltage LOW
+  }
   
+  // get duration based upon pot input
+  Serial.println(duration);
+  duration = analogRead(POT_PIN)*5.0/1023.0;
+  delay(500);
 }

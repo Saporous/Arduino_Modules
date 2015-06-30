ed7c4428c0f36d3f420d97fbb8f1d7aea88a3c46
Jeffrey C <chen.jeffrey.r@gmail.com>
2015-06-29 17:12:52 -0700

    added DSKDM_LED, need to update README

---
+++ DSKDM Arduino/DSKDM_LED/DSKDM_LED.ino
@@ -0,0 +1,46 @@
+#if not (defined(__AVR_ATmega328P__))
+  #error Your board type is not supported!
+#endif
+
+#if (defined(__AVR_ATmega328P__))
+  #define LED0 2
+  #define LED1 3
+  #define LED2 4
+  #define LED3 5
+  #define LED4 6
+  #define LED5 7
+  #define LED6 8
+  #define LED7 9
+  #define LED8 10
+  #define LED9 11
+  #define LED10 12
+  #define LED11 A0
+  #define LED12 A1
+  #define LED13 A2
+  #define LED14 A3 
+#endif
+
+int LED[15] = {LED0,LED1,LED2,LED3,LED4,LED5,LED6,LED7,LED8,LED9,LED10,LED11,LED12,LED13,LED14};
+int value = LOW;
+
+void setup() {
+  Serial.begin(9600);
+  for(int i = 0; i < 15; i++){
+    pinMode(LED[i], OUTPUT);
+  }
+}
+
+void loop() {
+  
+  for(int i = 0; i < 15; i++){
+    digitalWrite(LED[i], value);
+    delay(500);
+    Serial.print(LED[i]);
+    Serial.print(" Value: ");
+    Serial.println((int)value);
+  }
+  if(value == HIGH){
+    value = LOW;
+  }
+  else value = HIGH;
+}
